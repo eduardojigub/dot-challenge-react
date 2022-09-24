41ee50c8ff18a86eb145ac7e7700f4b6575df38c
eduardojigub <eduardobrunoit@gmail.com>
2022-09-23 21:59:24 -0300

    adding outlet to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Home from './pages/Home';
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Home />
+      <Outlet />
     </div>
   );
 }
